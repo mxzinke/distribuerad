95b7d9b22e60f5430056b7c0fff52d858bc8ad48
Maximilian Zinke <zinke@smark.de>
2021-01-05 19:10:02 +0100

    add benchmark for publishing events on multiple channels

--- events/publish_test.go
+++ events/publish_test.go
@@ -1,6 +1,8 @@
 package events
 
 import (
+	domain "distribuerad/interface"
+	"fmt"
 	"testing"
 )
 
@@ -12,4 +14,38 @@ func BenchmarkPublishOneChannel(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		channel.AddEvent("HELLO1234-Event")
 	}
+}
+
+func BenchmarkPublish4Channels(b *testing.B) {
+	benchmarkPublishNChannels(4, b)
+}
+
+func BenchmarkPublish10Channels(b *testing.B) {
+	benchmarkPublishNChannels(10, b)
+}
+
+func benchmarkPublishNChannels(n int, b *testing.B) {
+	channelStore := NewChannelStore()
+	onFinish := make(chan int, n+1)
+	var channels []domain.IChannel
+	for i := 0; i < n; i++ {
+		channels = append(channels, channelStore.AddChannel(fmt.Sprintf("test-%d", n)))
+	}
+
+	everyChannelEvents := b.N / n
+
+	b.ResetTimer()
+	for _, channel := range channels {
+		go benchmarkAtChannel(channel, onFinish, everyChannelEvents)
+	}
+	for i := 0; i < n; i++ {
+		<-onFinish
+	}
+}
+
+func benchmarkAtChannel(channel domain.IChannel, onFinish chan int, nEvents int) {
+	for i := 0; i < nEvents; i++ {
+		channel.AddEvent("HELLO1234-Event")
+	}
+	onFinish <- 1
 }
